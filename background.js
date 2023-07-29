517a7e3d9ab6555f2f0d588e9f253d15ed44e05d
Kubacki Wojciech <kubackiwo@gmail.com>
2023-07-29 16:37:40 +0200

    removed useless code

--- background.js
+++ background.js
@@ -12,14 +12,3 @@ setInterval(() => {
             document.body.append(styleElement);
         }
 }, 60000);
-
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.query({
-		currentWindow: true,
-		active: true
-	}, function(tab) {
-		chrome.tabs.create({
-			"url": "http://dev.opera.com"
-		});
-	});
-});
